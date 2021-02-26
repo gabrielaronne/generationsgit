6147ca6168cda9c6d33c76d4c3049a446679d7ae
unknown <gabiaronne@gmail.com>
2021-02-26 14:20:04 -0300

    exercicio condicional corrigido

--- java/exercicio2lacorepeticao.java
+++ java/exercicio2lacorepeticao.java
@@ -6,16 +6,15 @@ public class exercicio2lacorepeticao {
 
 	public static void main(String[] args) {
 
-		int idade, soma21anos = 0, soma50anos = 1;
+		int idade, soma21anos=0, soma50anos=0;
 		Scanner leia = new Scanner(System.in);
 
 		System.out.println("escreva aqui uma idade: ");
 		idade = leia.nextInt();
 
 		while (idade != -99) {
-			System.out.println("escreva aqui uma idade: ");
-			idade = leia.nextInt();
-
+			
+			
 			if (idade > 1 && idade < 22) {
 				soma21anos++;
 
@@ -25,10 +24,14 @@ public class exercicio2lacorepeticao {
 				soma50anos++;
 
 			}
+			
+			System.out.println("escreva aqui uma idade: ");
+			idade = leia.nextInt();
+
 		}
+		
 		System.out.printf("\n o numero de jovens 'e: %d", soma21anos);
 		System.out.printf("\n O numero de adultos : %d", soma50anos);
-		idade = leia.nextInt();
-
+		
 	}
 }
