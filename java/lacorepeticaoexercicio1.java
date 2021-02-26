c74675fc7b9cf52adcf8629ab7ea0fdf918548d2
unknown <gabiaronne@gmail.com>
2021-02-26 09:47:41 -0300

    lacorepeticao um exercicio

---
+++ java/lacorepeticaoexercicio1.java
@@ -0,0 +1,34 @@
+package PacoteJava;
+
+import java.util.Scanner;
+
+public class lacorepeticaoexercicio1 {
+
+	public static void main(String[] args) {
+		
+		int x,quan;
+		Scanner Leia = new Scanner(System.in);
+		
+	for (quan=1;quan<11;quan++) {
+		System.out.println("\n escreva seu numero: ");
+		x=Leia.nextInt();
+		
+	
+		
+		if (x%2==0) {
+         
+		System.out.printf("\n esse numero e par",x);}
+		
+		if (x%2!=0) {
+			
+			System.out.printf("\n esse numero e impar",x);}
+			
+			
+		}
+	}
+}		
+		
+
+	
+
+
