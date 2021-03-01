bcc38ebe7b0536eb9980eae872a833ff9ce10a49
unknown <gabiaronne@gmail.com>
2021-03-01 15:58:41 -0300

    correcao exercicio

---
+++ java/exerciciolistavetores2.java
@@ -0,0 +1,39 @@
+package PacoteJava;                                                                                                
+                                                                                                                   
+import java.util.Scanner;                                                                                          
+                                                                                                                   
+public class exerciciolistavetores2 {                                                                          
+                                                                                                                   
+	public static void main(String[] args) {                                                                       
+		int[] vetor = new int[6];                                                                                  
+		int somapar = 0, somaimpar = 0;                                                                            
+		;                                                                                                          
+                                                                                                                   
+		Scanner entrada = new Scanner(System.in);                                                                  
+                                                                                                                   
+		for (int x = 0; x < 6; x++) {                                                                              
+                                                                                                                   
+			System.out.println("insira aqui o seu numero: ");                                                      
+			vetor[x] = entrada.nextInt();                                                                          
+                                                                                                                   
+			if (vetor[x] % 2 == 0) {                                                                               
+                                                                                                                   
+				System.out.println("\n esse numero e par");                                                        
+				somapar = somapar + vetor[x];                                                                      
+			}                                                                                                      
+                                                                                                                   
+			if (vetor[x] % 2 != 0) {                                                                               
+                                                                                                                   
+				System.out.println("\n esse numero e impar");                                                      
+				somaimpar++;                                                                                       
+                                                                                                                   
+			}                                                                                                      
+                                                                                                                   
+		}                                                                                                          
+		System.out.printf("\n a somatoria dos numeros pares 'e : %d", somapar);                                    
+                                                                                                                   
+		System.out.printf("\n a quantidade de numeros impares digitados  'e : %d    ", somaimpar);                 
+	}                                                                                                              
+                                                                                                                   
+}                                                                                                                  
+                                                                                                                   
